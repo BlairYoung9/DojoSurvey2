d13d26d5a63eb62824ad4505e6b2125329c54b81
Blair Young <blairyoung009@gmail.com>
2021-12-04 12:43:29 -0800

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+namespace DojoSurvey.Controllers     //be sure to use your own project's namespace!
+{
+    public class HomeController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public ViewResult Index()
+        {
+            // ViewBag.MyName = "Blair";
+            // ViewBag.DojoLocation = "Bellevue";
+
+            return View();
+        }
+        [HttpPost("result")]
+        public ViewResult Result(string name, string dojo, string language, string comment)
+        {   
+            ViewBag.name = name;
+            ViewBag.dojo = dojo;
+            ViewBag.language = language;
+            ViewBag.comment = comment;
+            return View();
+
+        }
+        
+        
+        // Other route examples
+        [HttpGet]
+        [Route("{name}")]
+        public string Index(string name)
+        {
+            return $"Hello {name}!";
+        }
+
+        [HttpGet]
+        [Route("pizza/{topping}")]
+        public string PizzaParty(string topping)
+        {
+            return $"Who's ready for a {topping} Pizza!";
+        }
+        [HttpGet]
+        [Route("user/{name}/{location}/{age}")]
+        public string UserInfo(string name, string location, int age)
+        {
+            return $"{name}, aged {age}, is from {location}";
+        }
+    }
+}
